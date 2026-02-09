78da5afd95968a269ab34ed1532bfc30d9e3686a
nagasantosh <winthegoal@gmail.com>
2026-02-09 15:11:53 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { SiteShell } from "@/components/SiteShell";
 import { Card, CardContent } from "@/components/ui/Card";
